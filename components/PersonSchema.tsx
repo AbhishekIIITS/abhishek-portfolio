4c8c12b9f9b4ab9746306f98d70b32a0970e61cb
ABHISHEK SONKAR <sonkarabhishek958@gmail.com>
2026-09-25 16:03:12 +0530

    Update PersonSchema.tsx

--- components/PersonSchema.tsx
+++ components/PersonSchema.tsx
@@ -1,10 +1,16 @@
+
 export default function PersonSchema() {
   const schema = {
     "@context": "https://schema.org",
     "@type": "Person",
+    "@id": "https://abhisheksonkar.in/#person",
     name: "Abhishek Sonkar",
-    url: "https://abhishek-portfolio-git.vercel.app",
+    url: "https://abhisheksonkar.in",
     jobTitle: "Computer Science Engineering Student",
+    affiliation: {
+      "@type": "CollegeOrUniversity",
+      name: "Indian Institute of Information Technology Surat",
+    },
     sameAs: [
       "https://www.linkedin.com/in/abhishek-sonkar-2b7415328",
       "https://github.com/AbhishekIIITS",
