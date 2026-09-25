cfafd24ee4562f9f45e48864712a7a6b8d9cd69d
ABHISHEK SONKAR <sonkarabhishek958@gmail.com>
2026-09-25 15:00:56 +0530

    Fix URL quotes in sitemap function

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://abhisheksonkar.in",
+      url: 'https://abhisheksonkar.in',
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
